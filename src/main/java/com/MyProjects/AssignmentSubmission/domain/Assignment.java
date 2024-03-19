89b6fd9facc8344284ecad6dd2bfe55eb4291232
aymanwaell <aymanwael@icloud.com>
2024-03-19 17:05:43 +0200

    Initial commit

---
+++ src/main/java/com/MyProjects/AssignmentSubmission/domain/Assignment.java
@@ -0,0 +1,21 @@
+package com.MyProjects.AssignmentSubmission.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class Assignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+    private String githubUrl;
+    private String branch;
+    private String codeReviewVideoUrl;
+    @ManyToOne(optional = false)
+    private User user;
+}
---
+++ src/main/java/com/MyProjects/AssignmentSubmission/domain/User.java
@@ -0,0 +1,19 @@
+package com.MyProjects.AssignmentSubmission.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate cohortStartDate;
+    private String username;
+    private String password;
+}
